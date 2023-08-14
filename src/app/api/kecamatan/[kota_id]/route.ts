eb149e92be56a2f82dd5e7914771993d14fe5703
repirisyan <repirisyan@icloud.com>
2023-08-14 21:31:48 +0700

    fix deployment

--- src/app/api/kecamatan/[kota_id]/route.ts
+++ src/app/api/kecamatan/[kota_id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from 'fs';
 
-export async function GET(request, context: { params: { kota_id: string } }) {
+export async function GET(request: Request, context: { params: { kota_id: string } }) {
     const fileContents = await fs.readFile('./public/kecamatan.json', 'utf-8');
     const objectData = JSON.parse(fileContents);
     const filteredData = objectData.filter(kecamatan => kecamatan.kota_id == context.params.kota_id);
