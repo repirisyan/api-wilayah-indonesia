21a4e3e6a33e057f65262e1cd24ea09aad948d4a
repirisyan <repirisyan@icloud.com>
2023-08-14 21:45:52 +0700

    fix route

--- src/app/api/kecamatan/[kota_id]/route.ts
+++ src/app/api/kecamatan/[kota_id]/route.ts
@@ -4,6 +4,6 @@ import { promises as fs } from 'fs';
 export async function GET(request: Request, context: { params: { kota_id: string } }) {
     const fileContents = await fs.readFile('./public/kecamatan.json', 'utf-8');
     const objectData = JSON.parse(fileContents);
-    const filteredData = objectData.filter(kecamatan => kecamatan.kota_id == context.params.kota_id);
+    const filteredData = objectData.filter((kecamatan: any) => kecamatan.kota_id == context.params.kota_id);
     return NextResponse.json(filteredData, { status: 200 });
 }
--- src/app/api/kota/[provinsi_id]/route.ts
+++ src/app/api/kota/[provinsi_id]/route.ts
@@ -4,6 +4,6 @@ import { promises as fs } from 'fs';
 export async function GET(request: Request, context: { params: { provinsi_id: string } }) {
     const fileContents = await fs.readFile('./public/kota.json', 'utf-8');
     const objectData = JSON.parse(fileContents);
-    const filteredData = objectData.filter(kota => kota.provinsi_id == context.params.provinsi_id);
+    const filteredData = objectData.filter((kota: any) => kota.provinsi_id == context.params.provinsi_id);
     return NextResponse.json(filteredData, { status: 200 });
 }
