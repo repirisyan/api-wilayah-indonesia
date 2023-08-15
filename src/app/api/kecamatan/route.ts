eb77b0b6a62b621ef89f33131c31ccb43bffb2fe
repirisyan <repirisyan@icloud.com>
2023-08-15 15:36:14 +0700

    fix route

--- src/app/api/kecamatan/route.ts
+++ src/app/api/kecamatan/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { promises as fs } from 'fs';
+import path from "path";
 
 export async function GET(request: NextRequest) {
-    const fileContents = await fs.readFile('./public/kecamatan.json', 'utf-8');
+    const file = path.join(process.cwd(), 'public', 'kecamatan.json');
+    const fileContents = await fs.readFile(file, 'utf-8');
     const objectData = JSON.parse(fileContents);
     const filteredData = objectData.filter((kecamatan: any) => kecamatan.kota_id == request.nextUrl.searchParams.get('kota_id'));
     return NextResponse.json(filteredData, { status: 200 });
