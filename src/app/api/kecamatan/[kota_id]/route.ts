523e04eb21a82cbb4ec5664ebd002bc7583606f1
repirisyan <repirisyan@icloud.com>
2023-08-15 14:23:09 +0700

    add nextrequest on route

--- src/app/api/kecamatan/[kota_id]/route.ts
+++ src/app/api/kecamatan/[kota_id]/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { promises as fs } from 'fs';
 
-export async function GET(request: Request, context: { params: { kota_id: string } }) {
+export async function GET(request: NextRequest, context: { params: { kota_id: string } }) {
     const fileContents = await fs.readFile('./public/kecamatan.json', 'utf-8');
     const objectData = JSON.parse(fileContents);
     const filteredData = objectData.filter((kecamatan: any) => kecamatan.kota_id == context.params.kota_id);
--- src/app/api/provinsi/route.ts
+++ src/app/api/provinsi/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { promises as fs } from 'fs';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     const fileContents = await fs.readFile('./public/provinsi.json', 'utf-8');
     const objectData = JSON.parse(fileContents);
     return NextResponse.json(objectData, { status: 200 })
