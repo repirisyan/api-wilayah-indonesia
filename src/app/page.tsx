241bd71046d320b914889cfe693b19e3ef1744ac
repirisyan <repirisyan@icloud.com>
2023-08-15 12:38:45 +0700

    add clipboard and tooltip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,51 @@
+"use client"
 import Image from 'next/image'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import DocumentDuplicateIcon from "@heroicons/react/24/outline/DocumentDuplicateIcon";
+import { useState } from "react";
+
+const CopyBoard = (pre: any) => {
+  const [copyTip, setCopyTip] = useState("Copy code");
+  return (
+    <div className="relative">
+      <span className='absolute p-2'>
+        <small className='text-gray-100'>Method : GET</small>
+      </span>
+      <button
+        className="tooltip tooltip-left right-0 top-0 absolute p-2"
+        data-tip={copyTip}
+      >
+        <CopyToClipboard
+          text={pre.url}
+          onCopy={async () => {
+            setCopyTip("Copied");
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            setCopyTip(`Copy code`);
+          }}>
+          <DocumentDuplicateIcon className="h-5 w-5 cursor-pointer hover:text-blue-600" />
+        </CopyToClipboard>
+      </button>
+    </div>
+  );
+};
+
+const CodeBlock = (context: any) => {
+  return (
+    <div>
+      <CopyBoard url={context.code}></CopyBoard>
+      <SyntaxHighlighter
+        language=""
+        style={oneDark}
+        scrollbarClass="overflow-hidden"
+        children={context.code}
+      />
+    </div>
+  );
+};
+
+
 
 export default function Home() {
   return (
@@ -19,48 +66,28 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative mt-20 grid grid-cols-1 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="mt-20 grid grid-cols-1">
         <section>
           <h1>
-            GET Data Provinsi
+            Data Provinsi
           </h1>
-          <p>
-            <code className='font-bold'>https://wilayah-indonesia-api.vercel.app/api/provinsi</code>
-          </p>
+          <CodeBlock code={"\n https://region-indonesia.vercel.app/api/provinsi"} />
         </section>
         <section className='mt-5'>
           <h1>
-            GET Data Kota
+            Data Kota
           </h1>
-          <p><code className='font-bold'>https://wilayah-indonesia-api.vercel.app/api/kota/provinsi_id</code></p>
-          <p
-            className="inline-block mt-2 whitespace-nowrap rounded-[0.27rem] bg-neutral-800 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-neutral-50 dark:bg-neutral-900">
-            <div className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-              </svg>
-              &nbsp;provinsi_id di dapat dari id data provinsi
-            </div>
-          </p>
+          <CodeBlock code={"\n https://region-indonesia.vercel.app/api/kota/provinsi_id"} />
         </section>
         <section className='mt-5'>
           <h1>
-            GET Data Kecamatan
+            Data Kecamatan
           </h1>
-          <p><code className='font-bold'>https://wilayah-indonesia-api.vercel.app/api/kecamatan/kota_id</code></p>
-          <p
-            className="inline-block mt-2 whitespace-nowrap rounded-[0.27rem] bg-neutral-800 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-neutral-50 dark:bg-neutral-900">
-            <span className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-              </svg>
-              &nbsp;kota_id di dapat dari id data kota
-            </span>
-          </p>
+          <CodeBlock code={"\n https://region-indonesia.vercel.app/api/kecamatan/kota_id"} />
         </section>
       </div>
 
-      <div className="text-center mt-24">
+      <div className="text-center mt-14">
         <h2 className={`mb-3 text-2xl font-semibold`}>
           Powered By :
           <Image className='invert mx-auto my-3'
@@ -69,13 +96,11 @@ export default function Home() {
             height={80}
             alt="Vercel Logo"
           />
-
         </h2>
-
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
           &copy; 2023 repirisyan. All Right Reserved
         </p>
       </div>
-    </main>
+    </main >
   )
 }
