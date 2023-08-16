26253931de0b8ce5e750b13e2581baed96e9cc2e
repirisyan <repirisyan@icloud.com>
2023-08-16 21:41:57 +0700

    fix name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,11 +20,11 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://github.com/repirisyan98/"
+            href="https://nextjs.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By repirisyan
+            Next.JS
           </a>
         </div>
       </div>
@@ -114,7 +114,7 @@ export default function Home() {
       </dialog>
       <div className="text-center mt-14">
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          &copy; 2023 repirisyan. All Right Reserved
+          &copy; 2023 . All Right Reserved
         </p>
       </div>
     </main >
